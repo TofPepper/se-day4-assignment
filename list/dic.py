7cc8dbf6ab7590cc17094a58fcce573afb63a8e3
TofPepper <etofunmi3@gmail.com>
2024-08-23 14:13:32 +0100

    first commit

---
+++ list/dic.py
@@ -0,0 +1,5 @@
+fruits = ["Apple", "Banana", "Orange", "Cherry"]
+print(f"My favourite fruit is {fruits[3]}")
+food = ["Jollof rice and turkey", "Spagetti, turkey and plantain", "Amala abula", "Pounded yam and efo riro", "Soft boiled potatoes and beef stew", "Yam and egg", "Noodles"]
+print(f"The list of food I eat the most include: {food[0]}, {food[1]},  {food[2]}")
+print(f"I enjoy eating {food[1]}, most especially after eating {fruits[0]}")
